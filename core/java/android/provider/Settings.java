82088bdf1733e89bae53019b171ec36cab6bc2bf
Daniel Koman <dankoman30@gmail.com>
2015-07-17 20:32:20 +0200

    SystemUI: SlimDim - pref to listen on entire screen for touches [1/2]
    Change-Id: I0fdf697166c61d7ec6c7c01f1c76924d5617c492
    
    Conflicts:
    	core/java/android/provider/Settings.java

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3907,7 +3907,14 @@ public final class Settings {
           * @hide
           */
          public static final String DIM_NAV_BUTTONS_ANIMATE_DURATION = "dim_nav_buttons_animate_duration";
- 
+
+       /**
+         * Whether to listen on the entire screen for touches to un-dim
+         * the buttons instead of just listening on the navbar
+         * @hide
+         */
+        public static final String DIM_NAV_BUTTONS_TOUCH_ANYWHERE = "dim_nav_buttons_touch_anywhere";
+
        /**
          * @deprecated Use
          * {@link android.provider.Settings.Global#WIFI_MAX_DHCP_RETRY_COUNT} instead
