c038dc5bb1fc49fd9ccf1ea726110659747f7654
ghbhaha <ghbhaha@gmail.com>
2015-08-10 13:14:20 +0800

    [1/2]frameworks/base: Add three-fingers-swipe to screenshot
    candied by calebcabob <calphonic@gmail.com>
    The feature is ported from Oppo ColorOS.
    With the options on, users can swipe down
    on the screen to have a screenshot.
    
    Squashed reboot fix - https://github.com/zephiK/android_frameworks_baseLP/commit/408a6f728aec7974fe23ad635fa42475da49df04
    
    -Sources: SudaMod
     https://github.com/SudaMod
    
    -Original smali codes: wuxianlin
    -Adapted for Marshmallow: zephiK
    
    Change-Id: I6b676ac16e4748cd6781cd4f3f146f7c02a77db4

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4050,6 +4050,13 @@ public final class Settings {
          */
         public static final String STATUS_BAR_CUSTOM_HEADER_PROVIDER = "status_bar_custom_header_provider";
 
+         /**
+         * Three Finger Gesture from Oppo
+         *
+         * @hide
+         */
+        public static final String THREE_FINGER_GESTURE = "three_finger_gesture";
+
         /**
          * Enable/Disable screenshot sound
          * @hide
