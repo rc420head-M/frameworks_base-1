eca88710dec219f11f4bca6c96bf16aba9811c33
dwitherell <devonwitherell1@gmail.com>
2015-11-04 12:16:39 -0500

    Make network meter play nice with dark mode
    Signed-off-by: mydongistiny <jaysonedson@gmail.com>
    
    Conflicts:
    	packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBarIconController.java
    
    Change-Id: Ib0297f6b70cbf7767fa7822bb2da12c0f5545a67

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBarIconController.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBarIconController.java
@@ -47,6 +47,7 @@ import com.android.systemui.omni.BatteryViewManager;
 import com.android.systemui.statusbar.NotificationData;
 import com.android.systemui.statusbar.SignalClusterView;
 import com.android.systemui.statusbar.StatusBarIconView;
+import com.android.systemui.statusbar.policy.NetworkTraffic;
 import com.android.systemui.tuner.TunerService;
 import com.android.systemui.tuner.TunerService.Tunable;
 
@@ -80,6 +81,7 @@ public class StatusBarIconController extends StatusBarIconList implements Tunabl
     private ClockController mClockController;
     private BatteryViewManager mBatteryViewManager;
     private View mCenterClockLayout;
+    private NetworkTraffic mNetworkTraffic;
 
     private int mIconSize;
     private int mIconHPadding;
@@ -137,7 +139,7 @@ public class StatusBarIconController extends StatusBarIconList implements Tunabl
         //mBatteryMeterView = (BatteryMeterView) statusBar.findViewById(R.id.battery);
         mBatteryViewManager = phoneStatusBar.getBatteryViewManager();
         //scaleBatteryMeterViews(context);
-
+        mNetworkTraffic = (NetworkTraffic) statusBar.findViewById(R.id.networkTraffic);
         mDarkModeIconColorSingleTone = context.getColor(R.color.dark_mode_icon_color_single_tone);
         mLightModeIconColorSingleTone = context.getColor(R.color.light_mode_icon_color_single_tone);
         mHandler = new Handler();
@@ -534,6 +536,7 @@ public class StatusBarIconController extends StatusBarIconList implements Tunabl
         mSignalCluster.setIconTint(mIconTint, mDarkIntensity, mTintArea);
         mClockController.setTextColor(mIconTint);
         mBatteryViewManager.setDarkIntensity(mDarkIntensity);
+	    mNetworkTraffic.setDarkIntensity(mDarkIntensity);
     }
 
     public void appTransitionPending() {
