910be89301c75aa0f86629dfd31b7163deea3472
Evisceration <eviscerationls@gmail.com>
2015-12-28 16:46:59 -0500

    Squashed On-The-Go mode (1/2)
    On-The-Go turns on the camera of your phone and streams it to an overlay,
    which is drawn over everything.
    this makes you feel like your display is transparent, as you see the content
    behind it.
    
    its really useful when you are using your phone while walking, thats why
    we called it "On-The-Go"
    
    allows to change transparency of the overlay via the notification.
    
    have fun!
    
    Aicpfy:
    - Use Aicp utils
    - Proper icon color from Aicp LP
    
    Change-Id: Ibbe1e288c53cd7f53196f0a9110807d907738408
    
    Conflicts:
    	core/java/android/provider/Settings.java
    	core/java/com/android/internal/util/du/DuUtils.java
    	core/res/res/values/cm_strings.xml
    	packages/SystemUI/AndroidManifest.xml
    	services/core/java/com/android/server/policy/GlobalActions.java

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4109,6 +4109,36 @@ public final class Settings {
         public static final String STATUSBAR_BATTERY_BAR = "statusbar_battery_bar";
 
         /**
+         * If On-The-Go should be displayed at the power menu.
+         *
+         * @hide
+         */
+        public static final String POWER_MENU_ONTHEGO_ENABLED = "power_menu_onthego_enabled";
+
+        /**
+         * The alpha value of the On-The-Go overlay.
+         *
+         * @hide
+         */
+        public static final String ON_THE_GO_ALPHA = "on_the_go_alpha";
+
+        /**
+         * The camera instance to use.
+         * 0 = Rear Camera
+         * 1 = Front Camera
+         *
+         * @hide
+         */
+        public static final String ON_THE_GO_CAMERA = "on_the_go_camera";
+
+        /**
+         * Whether the service should restart itself or not.
+         *
+         * @hide
+         */
+        public static final String ON_THE_GO_SERVICE_RESTART = "on_the_go_service_restart";
+
+        /**
          * @hide
          */
         public static final String STATUSBAR_BATTERY_BAR_COLOR = "statusbar_battery_bar_color";
