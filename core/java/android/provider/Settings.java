47720737a23e8c13a069e89593553fd7f7a6c42d
Tim Klein <cuzz1369@gmail.com>
2016-01-04 00:39:31 -0500

    Fix mismerge in Settings.java

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3684,7 +3684,6 @@ public final class Settings {
         public static final String NAVIGATION_BAR_SHOW = "navigation_bar_show";
 
         /**
-<<<<<<< HEAD
          * Whether to show the battery bar
          * @hide
          */
@@ -3709,12 +3708,13 @@ public final class Settings {
          * @hide
          */
         public static final String STATUSBAR_BATTERY_BAR_ANIMATE = "statusbar_battery_bar_animate";
-=======
+
+        /*
          * Whether the notification light will be allowed when in zen mode during downtime
          * @hide
          */
         public static final String ALLOW_LIGHTS = "allow_lights";
->>>>>>> fc2f2c9... Frameworks: Allow/Prevent notification light in Zen mode (1 of 2)
+
 
 		/**
         * Network traffic indicator, goes from least to greatest significant bitwise
