97acaf08cf7aec66d20708c86e234aca4be6f7bf
Tim Klein <cuzz1369@gmail.com>
2016-01-10 18:45:38 -0500

    Revert "Frameworks/Systemui DayNight update"
    This reverts commit 6355f3877ee93dc9fc315876c6f115e9816077f9.

--- services/core/java/com/android/server/power/ShutdownThread.java
+++ services/core/java/com/android/server/power/ShutdownThread.java
@@ -357,7 +357,7 @@ public final class ShutdownThread extends Thread {
         }
 
         // Throw up a system dialog to indicate the device is rebooting / shutting down.
-        ProgressDialog pd = new ProgressDialog(context, com.android.internal.R.style.Theme_Material_DayNight_Dialog_Alert);
+        ProgressDialog pd = new ProgressDialog(context);
 
         // Path 1: Reboot to recovery and install the update
         //   Condition: mRebootReason == REBOOT_RECOVERY and mRebootUpdate == True
@@ -880,4 +880,3 @@ public final class ShutdownThread extends Thread {
         }
     }
 }
-
