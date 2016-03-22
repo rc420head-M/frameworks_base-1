db5e196f0d4ddda0746bbb90784d6195bc79a28d
d34d <clark@cyngn.com>
2016-03-22 10:54:49 -0700

    Themes: Update icon mapping on boot Ensure icon mappings are applied on boot, otherwise themed icons will not be picked up until a theme change occurs.
    Change-Id: Iacda02c11d946e6f779d20739a6b2b67f407d6ba
    TICKET: CYNGNOS-2434

--- services/core/java/com/android/server/pm/PackageManagerService.java
+++ services/core/java/com/android/server/pm/PackageManagerService.java
@@ -17587,6 +17587,8 @@ public class PackageManagerService extends IPackageManager.Stub {
         for (String themePkgName : themesToProcess) {
             processThemeResources(themePkgName);
         }
+
+        updateIconMapping(themeConfig.getIconPackPkgName());
     }
 
     /**
