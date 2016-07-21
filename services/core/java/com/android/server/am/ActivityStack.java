2d81ad8dec9d591e3bc1f3615604779446344a1e
Andrii Kulian <akulian@google.com>
2016-07-20 18:59:28 -0700

    DO NOT MERGE -- Correctly finish activity in non-focused stack
    When activity is finished we first looked for next activity to
    show in focused stack. If real next activity to show in place
    of finishing one is in the same non-focused stack, we didn't
    fully complete the dismissal process and activity was stuck in
    FINISHING state.
    This CL checks if we're trying to finish visible activity in
    paused state and destroy it immediately if top running activity
    is visible - same as we do for pinned activities.
    
    Bug: 29458854
    Change-Id: I0d5ceb2daa45c0628d89417c8456e132996bcea9

--- services/core/java/com/android/server/am/ActivityStack.java
+++ services/core/java/com/android/server/am/ActivityStack.java
@@ -3614,16 +3614,16 @@ final class ActivityStack {
 
         if (mode == FINISH_IMMEDIATELY
                 || (prevState == ActivityState.PAUSED
-                    && (mode == FINISH_AFTER_PAUSE || mStackId == PINNED_STACK_ID))
-                || finishingActivityInNonFocusedStack
+                    && (mode == FINISH_AFTER_PAUSE || mode == FINISH_AFTER_VISIBLE
+                        || mStackId == PINNED_STACK_ID))
                 || prevState == ActivityState.STOPPED
                 || prevState == ActivityState.INITIALIZING) {
             r.makeFinishingLocked();
             boolean activityRemoved = destroyActivityLocked(r, true, "finish-imm");
 
-            if (finishingActivityInNonFocusedStack) {
-                // Finishing activity that was in paused state and it was in not currently focused
-                // stack, need to make something visible in its place.
+            if (prevState == ActivityState.PAUSED && mode == FINISH_AFTER_VISIBLE) {
+                // Finishing activity that was in paused state - this can happen if it was in
+                // not currently focused stack. Need to make something visible in its place.
                 mStackSupervisor.ensureActivitiesVisibleLocked(null, 0, !PRESERVE_WINDOWS);
             }
             if (activityRemoved) {
